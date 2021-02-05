5f3e21b64508085f505f321672cde323caacd0c8
Sara V <saravillanueva1333@gmail.com>
2021-02-04 23:13:45 -0800

    Adding Interview Project

---
+++ RandomList.UnitTest/UnitTest1.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+
+namespace RandomList_UnitTests
+{
+    [TestClass]
+    public class RandomListTests
+    {
+        [TestMethod]
+        public void Shuffle_NotRandom_ShouldThrowException()
+        {
+            //Arrange
+            List<int> list1 = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            List<int> list2 = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            //Act
+            RandomizeListNS.RandomList.Shuffle(list1);
+            RandomizeListNS.RandomList.Shuffle(list2);
+
+            //Assert
+            CollectionAssert.AreNotEqual(list1, list2);
+
+        }
+
+        [TestMethod]
+        public void MakeRandomList_ItemsOutOfLimit_ShouldThrowException()
+        {
+            List<int> list = RandomizeListNS.RandomList.MakeRandomList(8);
+
+            CollectionAssert.DoesNotContain(list, 8);
+
+        }
+
+        [TestMethod]
+        public void MakeRandomList_ItemsNotUnique_ShouldThrowException()
+        {
+            List<int> list = RandomizeListNS.RandomList.MakeRandomList(12);
+
+            CollectionAssert.AllItemsAreUnique(list);
+
+        }
+    }
+}
---
+++ RandomizeList/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomizeListNS
+{
+    public class RandomList
+    {
+        public static void Main(string[] args)
+        {
+            var result = MakeRandomList(13);
+            Console.WriteLine(String.Join(",",result));
+
+        }
+
+        public static List<int> MakeRandomList(int userInput)
+        {
+            //Throw exception if input is negative
+            if (userInput < 0)
+                throw new ArgumentException("Input must be positive");
+
+            //Build List of unique integers
+            List<int> list = new List<int>();
+            for(int i = 0; i < userInput; i++ ){
+                list.Add(i);
+            }
+
+            //Now shuffle the list if more than 1 item
+            if (userInput > 1)
+                Shuffle(list);
+
+            return list;
+        }
+
+        public static void Shuffle (List<int> orderedlist)
+        {
+            for(int i = 0; i < orderedlist.Count; i++)
+            {
+                //Generate random number and swap it with i within the list
+                int randomNum = new Random().Next(orderedlist.Count);
+                int temp = orderedlist[randomNum];
+                orderedlist[randomNum] = orderedlist[i];
+                orderedlist[i] = temp;
+            }
+        }
+    }
+}
